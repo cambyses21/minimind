b54ed2d7f7b3e13dbb0eae4e9a77749600bf9c46
cambyses21 <cambyses21@protonmail.com>
2022-02-11 09:34:00 +0100

    Caesar cypher function

--- messagemixer.js
+++ messagemixer.js
@@ -17,7 +17,25 @@ Here is your encrypted message:
 > olleh dlrow
 */
 
+function shiftedKey(shift){
+    const alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    const shifted = [...alpha];
+    const key = {};
+    Array.from({length: shift}, () => {
+        shifted.push(shifted.shift());
+    });
+    for (let i = 0; i < alpha.length; i++) {
+        key[alpha[i]] = shifted[i];
+    };
+    return key;
+}
+
 function caesarCypher(message, shift) {
+    const key = shiftedKey(shift);
+
+    return message.toLowerCase().split('').map(elem => {
+        return key[elem] ? key[elem]: elem;
+    }).join('');
 
 }
 
@@ -26,9 +44,12 @@ function symbolCypher(message){
 }
 
 function reverseCypher(message){
-    return message.split(' ').reverse();
+    return message.split(' ').map(elem => elem.split('').reverse().join('')).join(' ');
     //arr.map(elem => elem.split().sort((a, b) => b - a).join());
 }
 
 let test = 'A test sentence for encoding';
 console.log(reverseCypher(test));
+console.log(symbolCypher(test));
+console.log(caesarCypher(test,1));
+
